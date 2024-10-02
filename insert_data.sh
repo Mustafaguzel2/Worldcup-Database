b8d9875509d782815b6e8506d699f002da566e76
Mustafa Yiğit Güzel <mustafaguzel879@gmail.com>
2024-10-02 11:40:24 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -6,5 +6,34 @@ then
 else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
+echo "$($PSQL "TRUNCATE TABLE games, teams")"
+cat games.csv  | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  # Başlık satırını atla
+  if [[ $YEAR != 'year' ]]
+  then
+    # WINNER ID'sini al
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    
+    # Eğer WINNER bulunamadıysa, ekle ve yeni ID'yi al
+    if [[ -z $WINNER_ID ]]
+    then
+      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    fi
 
+    # OPPONENT ID'sini al
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    
+    # Eğer OPPONENT bulunamadıysa, ekle ve yeni ID'yi al
+    if [[ -z $OPPONENT_ID ]]
+    then
+      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    fi
+
+    # games tablosuna verileri ekle
+    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES('$YEAR', '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+  fi
+done
 # Do not change code above this line. Use the PSQL variable above to query your database.
